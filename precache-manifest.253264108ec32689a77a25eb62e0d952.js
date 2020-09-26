dd872e066ac1a7de6420838d04136ddebbd6c463
hangxuanyuan <yuan_0817@live.com>
2020-09-27 00:18:08 +0800

    Updates

---
+++ precache-manifest.253264108ec32689a77a25eb62e0d952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0975dee586aaf31fdca2a792dce147b",
+    "url": "/react-practiceform/index.html"
+  },
+  {
+    "revision": "10d661910790d6ab9a64",
+    "url": "/react-practiceform/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c551ccded50ebf50318c",
+    "url": "/react-practiceform/static/js/2.64d5ebf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practiceform/static/js/2.64d5ebf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d661910790d6ab9a64",
+    "url": "/react-practiceform/static/js/main.4a68e610.chunk.js"
+  },
+  {
+    "revision": "5839f6984cd84512c001",
+    "url": "/react-practiceform/static/js/runtime-main.e81188eb.js"
+  }
+]);
